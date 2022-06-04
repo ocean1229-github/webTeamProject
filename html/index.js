e134dd118649db61c153dc94380e7a640f974f92
ocean1229 <ocean1229@naver.com>
2022-06-05 04:49:48 +0900

    nogada start

---
+++ html/index.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+ $('#beauty').click(function() {
+  $('#popbeautyinside').show();
+ });
+
+ $('.close').click(function() {
+  $('#popbeautyinside').hide();
+ });
+});
+
+$(document).ready(function() {
+    $('#delluna').click(function() {
+        $('#popHotelDelLuna').show();
+    })
+
+    $('.close').click(function() {
+        $('#popHotelDelLuna').hide();
+    })
+})
