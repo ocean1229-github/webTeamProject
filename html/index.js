d95c76b0529ebe17e9092bdbf67d32533775b99e
nahy-512 <hedghog1004@naver.com>
2022-06-06 17:57:40 +0900

    Netflix POP

--- html/index.js
+++ html/index.js
@@ -1,22 +1,374 @@
+/* Netflix */
+	/*drama*/
 $(document).ready(function() {
- $('#beauty').click(function() {
-  $('#popbeautyinside').show();
- });
-
- $('.close').click(function() {
-  $('#popbeautyinside').hide();
- });
+  $('#sunshine').click(function() {
+    $('#popMrSunshine').show();
+  });
+  $('.close').click(function() {
+    $('#popMrSunshine').hide();
+  });
 });
-
+$(document).ready(function() {
+  $('#beauty').click(function() {
+    $('#popbeautyinside').show();
+  });
+  $('.close').click(function() {
+    $('#popbeautyinside').hide();
+  });
+});
 $(document).ready(function() {
     $('#delluna').click(function() {
         $('#popHotelDelLuna').show();
-    })
-
+    });
     $('.close').click(function() {
         $('#popHotelDelLuna').hide();
-    })
-})
+    });
+});
+$(document).ready(function() {
+    $('#summer').click(function() {
+        $('#popOurBelovedSummer').show();
+    });
+    $('.close').click(function() {
+        $('#popOurBelovedSummer').hide();
+    });
+});
+$(document).ready(function() {
+    $('#2521').click(function() {
+        $('#pop2521').show();
+    });
+    $('.close').click(function() {
+        $('#pop2521').hide();
+    });
+});
+$(document).ready(function() {
+    $('#friends').click(function() {
+        $('#popFriends').show();
+    });
+    $('.close').click(function() {
+        $('#popFriends').hide();
+    });
+});
+$(document).ready(function() {
+    $('#prison').click(function() {
+        $('#popPrisonPlaybook').show();
+    });
+    $('.close').click(function() {
+        $('#popPrisonPlaybook').hide();
+    });
+});
+$(document).ready(function() {
+    $('#hospital').click(function() {
+        $('#popHospitalPlaylist').show();
+    });
+    $('.close').click(function() {
+        $('#popHospitalPlaylist').hide();
+    });
+});
+$(document).ready(function() {
+    $('#sky').click(function() {
+        $('#popSKYCastle').show();
+    });
+    $('.close').click(function() {
+        $('#popSKYCastle').hide();
+    });
+});
+$(document).ready(function() {
+    $('#firstlife').click(function() {
+        $('#popBecauseThisIsMyFirstLife').show();
+    });
+    $('.close').click(function() {
+        $('#popBecauseThisIsMyFirstLife').hide();
+    });
+});
+$(document).ready(function() {
+    $('#18again').click(function() {
+        $('#pop18Again').show();
+    });
+    $('.close').click(function() {
+        $('#pop18Again').hide();
+    });
+});
+$(document).ready(function() {
+    $('#marryworld').click(function() {
+        $('#popTheWorldoftheMarried').show();
+    });
+    $('.close').click(function() {
+        $('#popTheWorldoftheMarried').hide();
+    });
+});
+$(document).ready(function() {
+    $('#guardian').click(function() {
+        $('#popGuardian').show();
+    });
+    $('.close').click(function() {
+        $('#popGuardian').hide();
+    });
+});
+$(document).ready(function() {
+    $('#never').click(function() {
+        $('#popNevertheless').show();
+    });
+    $('.close').click(function() {
+        $('#popNevertheless').hide();
+    });
+});
+
+$(document).ready(function() {
+    $('#gossip').click(function() {
+        $('#popGossipGirl').show();
+    });
+    $('.close').click(function() {
+        $('#popGossipGirl').hide();
+    });
+});
+
+	/*movie*/
+$(document).ready(function() {
+    $('#abouttime').click(function() {
+        $('#popAboutTime').show();
+    });
+    $('.close').click(function() {
+        $('#popAboutTime').hide();
+    });
+});
+$(document).ready(function() {
+    $('#lala').click(function() {
+        $('#popLaLaLand').show();
+    });
+    $('.close').click(function() {
+        $('#popLaLaLand').hide();
+    });
+});
+$(document).ready(function() {
+    $('#parasite').click(function() {
+        $('#popParasite').show();
+    });
+    $('.close').click(function() {
+        $('#popParasite').hide();
+    });
+});
+$(document).ready(function() {
+    $('#littlewomen').click(function() {
+        $('#popLittleWomen').show();
+    });
+    $('.close').click(function() {
+        $('#popLittleWomen').hide();
+    });
+});
+$(document).ready(function() {
+    $('#eternalsunshine').click(function() {
+        $('#popEternalSunshine').show();
+    });
+    $('.close').click(function() {
+        $('#popEternalSunshine').hide();
+    });
+});
+$(document).ready(function() {
+    $('#assassination').click(function() {
+        $('#popAssassination').show();
+    });
+    $('.close').click(function() {
+        $('#popAssassination').hide();
+    });
+});
+$(document).ready(function() {
+    $('#3idiots').click(function() {
+        $('#pop3Idiots').show();
+    });
+    $('.close').click(function() {
+        $('#pop3Idiots').hide();
+    });
+});
+$(document).ready(function() {
+    $('#trumanshow').click(function() {
+        $('#popTheTrumanShow').show();
+    });
+    $('.close').click(function() {
+        $('#popTheTrumanShow').hide();
+    });
+});
+$(document).ready(function() {
+    $('#kiss').click(function() {
+        $('#popFallinLoveatFirstKiss').show();
+    });
+    $('.close').click(function() {
+        $('#popFallinLoveatFirstKiss').hide();
+    });
+});
+$(document).ready(function() {
+    $('#ourtimes').click(function() {
+        $('#popOurTimes').show();
+    });
+    $('.close').click(function() {
+        $('#popOurTimes').hide();
+    });
+});
+$(document).ready(function() {
+    $('#venom1').click(function() {
+        $('#popVenom1').show();
+    });
+    $('.close').click(function() {
+        $('#popVenom1').hide();
+    });
+});
+$(document).ready(function() {
+    $('#venom2').click(function() {
+        $('#popVenom2').show();
+    });
+    $('.close').click(function() {
+        $('#popVenom2').hide();
+    });
+});
+$(document).ready(function() {
+    $('#spider').click(function() {
+        $('#popSpiderMan').show();
+    });
+    $('.close').click(function() {
+        $('#popSpiderMan').hide();
+    });
+});
+$(document).ready(function() {
+    $('#imitation').click(function() {
+        $('#popTheImitationGame').show();
+    });
+    $('.close').click(function() {
+        $('#popTheImitationGame').hide();
+    });
+});
+$(document).ready(function() {
+    $('#withgod').click(function() {
+        $('#popAlongwiththeGods').show();
+    });
+    $('.close').click(function() {
+        $('#popAlongwiththeGods').hide();
+    });
+});
+
+	/*original*/
+$(document).ready(function() {
+    $('#alldead').click(function() {
+        $('#popAllofUsAreDead').show();
+    });
+    $('.close').click(function() {
+        $('#popAllofUsAreDead').hide();
+    });
+});
+$(document).ready(function() {
+    $('#squid').click(function() {
+        $('#popSquidGame').show();
+    });
+    $('.close').click(function() {
+        $('#popSquidGame').hide();
+    });
+});
+$(document).ready(function() {
+    $('#queensgambit').click(function() {
+        $('#popTheQueensGambit').show();
+    });
+    $('.close').click(function() {
+        $('#popTheQueensGambit').hide();
+    });
+});
+$(document).ready(function() {
+    $('#bridgerton1').click(function() {
+        $('#popBridgerton1').show();
+    });
+    $('.close').click(function() {
+        $('#popBridgerton1').hide();
+    });
+});
+$(document).ready(function() {
+    $('#bridgerton2').click(function() {
+        $('#popBridgerton2').show();
+    });
+    $('.close').click(function() {
+        $('#popBridgerton2').hide();
+    });
+});
+$(document).ready(function() {
+    $('#strange1').click(function() {
+        $('#popStrangerThings1').show();
+    });
+    $('.close').click(function() {
+        $('#popStrangerThings1').hide();
+    });
+});
+$(document).ready(function() {
+    $('#strange2').click(function() {
+        $('#popStrangerThings2').show();
+    });
+    $('.close').click(function() {
+        $('#popStrangerThings2').hide();
+    });
+});
+$(document).ready(function() {
+    $('#strange3').click(function() {
+        $('#popStrangerThings3').show();
+    });
+    $('.close').click(function() {
+        $('#popStrangerThings3').hide();
+    });
+});
+$(document).ready(function() {
+    $('#strange4').click(function() {
+        $('#popStrangerThings4').show();
+    });
+    $('.close').click(function() {
+        $('#popStrangerThings4').hide();
+    });
+});
+$(document).ready(function() {
+    $('#dp').click(function() {
+        $('#popDP').show();
+    });
+    $('.close').click(function() {
+        $('#popDP').hide();
+    });
+});
+$(document).ready(function() {
+    $('#money').click(function() {
+        $('#popMoneyHeist').show();
+    });
+    $('.close').click(function() {
+        $('#popMoneyHeist').hide();
+    });
+});
+$(document).ready(function() {
+    $('#hell').click(function() {
+        $('#popHellbound').show();
+    });
+    $('.close').click(function() {
+        $('#popHellbound').hide();
+    });
+});
+$(document).ready(function() {
+    $('#human').click(function() {
+        $('#popExtracurricular').show();
+    });
+    $('.close').click(function() {
+        $('#popExtracurricular').hide();
+    });
+});
+$(document).ready(function() {
+    $('#kissing1').click(function() {
+        $('#popTheKissingBooth1').show();
+    });
+    $('.close').click(function() {
+        $('#popTheKissingBooth1').hide();
+    });
+});
+$(document).ready(function() {
+    $('#kissing2').click(function() {
+        $('#popTheKissingBooth2').show();
+    });
+    $('.close').click(function() {
+        $('#popTheKissingBooth2').hide();
+    });
+});
+
+
+
+
 $(document).ready(function() {
     $('#liberation').click(function() {
         $('#popliberation').show();
@@ -26,3 +378,12 @@ $(document).ready(function() {
         $('#popliberation').hide();
     })
 })
+
+$(document).ready(function() {
+    $('#csi').click(function() {
+        $('#popcsi').show();
+    })
+$('.close').click(function() {
+        $('#popcsi').hide();
+    });
+});
